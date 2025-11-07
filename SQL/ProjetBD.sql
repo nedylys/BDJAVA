5fe6de9bbffcf351226a3fa346deebcf6cd3078f
Hiba Hanini <hiba.hanini@grenoble-inp.org>
2025-11-07 12:42:35 +0100

    Tables SQL created

--- SQL/ProjetBD.sql
+++ SQL/ProjetBD.sql
@@ -45,6 +45,217 @@ CREATE TABLE ProducteurAPourActivité(
         ON DELETE Cascade
 );
 
+CREATE TABLE Produit(
+    idProduit INT PRIMARY KEY,
+
+    NomProduit VARCHAR(30),
+    CatégorieProduit VARCHAR(30),
+    DescriptionProduit VARCHAR(255),
+    StockProduit FLOAT,
+    idProducteur INT,
+
+    CONSTRAINT fk_produit_producteur
+        FOREIGN KEY (idProducteur) 
+        REFERENCES Producteur(idProducteur)
+        ON DELETE Cascade
+);
+
+
+CREATE TABLE SaisonDisponibilité(
+    DateDébut DATE,
+    DateFin DATE,
+
+    PRIMARY KEY (DateDébut, DateFin)
+)
+
+CREATE TABLE Caractéristique(
+    nomCaractéristique VARCHAR(50) PRIMARY KEY,
+    valeur VARCHAR(50),
+);
+
+CREATE TABLE ProduitAPourCaractéristique(
+    idProduit INT,
+    nomCaractéristique VARCHAR(50),
+
+    PRIMARY KEY (idProduit, nomCaractéristique),
+
+    CONSTRAINT fk_idProduit
+        FOREIGN KEY (idProduit) 
+        REFERENCES Produit(idProduit)
+        ON DELETE Cascade,
+    
+    
+    CONSTRAINT fk_nomCaractéristique
+        FOREIGN KEY (nomCaractéristique) 
+        REFERENCES Caractéristique(nomCaractéristique)
+        ON DELETE Cascade
+);
+
+CREATE TABLe ProduitAPourSaison(
+    idProduit INT,
+    DateDébut DATE,
+    DateFin DATE,
+
+    PRIMARY KEY (idProduit, DateDébut, DateFin),
+
+    CONSTRAINT fk_idProduit_saison
+        FOREIGN KEY (idProduit) 
+        REFERENCES Produit(idProduit)
+        ON DELETE Cascade,
+    
+    
+    CONSTRAINT fk_saison_disponibilité
+        FOREIGN KEY (DateDébut, DateFin) 
+        REFERENCES SaisonDisponibilité(DateDébut, DateFin)
+        ON DELETE Cascade
+);
+
+CREATE TABLE ClientAnonyme(
+    idClient INT PRIMARY KEY,
+)
+
+
+CREATE TABLE Client(
+    emailClient VARCHAR(30) PRIMARY KEY,
+    NomClient VARCHAR(30),
+    PrenomClient VARCHAR(30),
+    TelephoneClient VARCHAR(15),
+    idClient INT,
+
+    CONSTRAINT fk_client_anonyme
+        FOREIGN KEY (idClient) 
+        REFERENCES ClientAnonyme(idClient)
+        ON DELETE Cascade
+);
+
+CREATE TABLE AdresseLivraison(
+    AdresseLivraison VARCHAR(255) PRIMARY KEY,
+)
+
+CREATE TABLE ClientAPourAdresseLivraison(
+    emailClient VARCHAR(30),
+    AdresseLivraison VARCHAR(255),
+
+    PRIMARY KEY (emailClient, AdresseLivraison),
+
+    CONSTRAINT fk_email_client
+        FOREIGN KEY (emailClient) 
+        REFERENCES Client(emailClient)
+        ON DELETE Cascade,
+    
+    
+    CONSTRAINT fk_adresse_livraison
+        FOREIGN KEY (AdresseLivraison) 
+        REFERENCES AdresseLivraison(AdresseLivraison)
+        ON DELETE Cascade
+);
+
+
+CREATE TABLE Commande(
+    idCommande INT PRIMARY KEY,
+    DateCommande DATE,
+    HeureCommande TIME,
+    ModePaiement VARCHAR(30),
+    ModeRecuperation VARCHAR(30),
+    idClient INT,
+
+    CONSTRAINT fk_commande_client
+        FOREIGN KEY (idClient) 
+        REFERENCES ClientAnonyme(idClient)
+        ON DELETE Cascade
+);
+
+
+CREATE TABLE Contenant(
+    idContenant INT PRIMARY KEY,
+    TypeContenant VARCHAR(30),
+    CapacitéContenant FLOAT,
+    StockDisponible INT,
+    ReutilisableContenant BOOLEAN
+);
+
+
+CREATE TABLE LotContenant(
+    DateRéceptionC DATE,
+    idContenant INT,
+    QuantitéDisponibleC INT,
+    PrixVenteCTTC FLOAT,
+
+    PRIMARY KEY (DateRéceptionC, idContenant),
+
+    CONSTRAINT fk_lot_contenant
+        FOREIGN KEY (idContenant) 
+        REFERENCES Contenant(idContenant)
+        ON DELETE Cascade
+);
+
+CREATE TABLE LotProduit(
+    ModeConditionnement VARCHAR(30),
+    PoidsUnitaire FLOAT,
+    DateRéceptionP DATE,
+    QuantitéDisponibleP INT,
+    DatePéremptionType VARCHAR(30),
+    DatePeremption DATE,
+    PrixVentePTTC FLOAT,
+    PrixAchatProducteur FLOAT,
+    idProduit INT,
+
+    PRIMARY KEY (DateRéceptionP, idProduit, ModeConditionnement, PoidsUnitaire),
+
+    CONSTRAINT fk_lot_produit
+        FOREIGN KEY (idProduit) 
+        REFERENCES Produit(idProduit)
+        ON DELETE Cascade
+);
+
+
+CREATE TABLE LigneCommandeProduit(
+    numLigneP INT,
+    idCommande INT,
+    idProduit INT,
+    ModeConditionnement VARCHAR(30),
+    PoidsUnitaire FLOAT,
+    DateRéceptionP DATE,
+    QuantitéCommandéeP FLOAT,
+    PrixUnitaireP FLOAT,
+    SousTotalLigneP FLOAT,
+
+    PRIMARY KEY (numLigneP, idCommande),
+
+    CONSTRAINT fk_ligne_commande_produit_commande
+        FOREIGN KEY (idCommande) 
+        REFERENCES Commande(idCommande)
+        ON DELETE Cascade,
+
+    CONSTRAINT fk_ligne_commande_produit_lot
+        FOREIGN KEY (DateRéceptionP, idProduit, ModeConditionnement, PoidsUnitaire) 
+        REFERENCES LotProduit(DateRéceptionP, idProduit, ModeConditionnement, PoidsUnitaire)
+        ON DELETE Cascade
+);
+
+
+CREATE TABLE LigneCommandeContenant(
+    numLigneC INT,
+    idCommande INT,
+    idContenant INT,
+    DateRéceptionC DATE,
+    QuantitéCommandéeC INT,
+    PrixUnitaireC FLOAT,
+    SousTotalLigneC FLOAT,
+
+    PRIMARY KEY (numLigneC, idCommande),
+
+    CONSTRAINT fk_ligne_commande_contenant_commande
+        FOREIGN KEY (idCommande) 
+        REFERENCES Commande(idCommande)
+        ON DELETE Cascade,
+
+    CONSTRAINT fk_ligne_commande_contenant_lot
+        FOREIGN KEY (DateRéceptionC, idContenant) 
+        REFERENCES LotContenant(DateRéceptionC, idContenant)
+        ON DELETE Cascade
+);
+
 -- DROP TABLE ProducteurAPourActivité;
 -- DROP TABLE Producteur;
 -- DROP TABLE Activité;
