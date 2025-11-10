cc85c0b29611efe3c34813edad5f8373b8719b03
Hiba Hanini <hiba.hanini@grenoble-inp.org>
2025-11-10 12:09:31 +0100

    Ajout de requêtes SQL pour tester si tout va bien

---
+++ SQL/test.sql
@@ -0,0 +1,17 @@
+SELECT * FROM AdressePComplete;
+SELECT * FROM Producteur;
+SELECT * FROM Produit;
+SELECT * FROM Commande;
+
+-- Produits Bio
+SELECT p.nomProduit
+FROM Produit p
+JOIN Caractéristique c ON p.idProduit = c.idProduit
+WHERE c.nomCaractéristique = 'bio' AND c.valeur = 'oui';
+
+-- Commande par Client 
+SELECT cl.nomClient, COUNT(*) AS nbCommandes
+FROM Client cl
+JOIN Commande c ON cl.idClient = c.idClient
+GROUP BY cl.nomClient;
+
