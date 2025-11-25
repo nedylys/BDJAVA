b63dbc3e475eebfb7be8bca737a631ccedbf8908
Hiba Hanini <hiba.hanini@grenoble-inp.org>
2025-11-25 14:47:12 +0100

    test

--- SQL/test.sql
+++ SQL/test.sql
@@ -16,3 +16,11 @@ JOIN Commande c ON cl.idClient = c.idClient
 GROUP BY cl.nomClient;
 
 SELECT * FROM Produit;
+
+SELECT l.idproduit, l.modeconditionnement, p.nomproduit, sum(l.quantitedisponiblep)
+FROM lotproduit l, produit p
+where l.idproduit = p.idproduit
+group by (l.idproduit, l.modeconditionnement, p.nomproduit);
+
+SELECT *
+FROM lotproduit;
