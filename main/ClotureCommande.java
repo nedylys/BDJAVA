9daf98faa060e3bc52b18183bf03130d93d68254
Jmeili <Makhtour.j-meili@grenoble-inp.org>
2025-11-29 11:01:48 +0100

    hh

--- main/ClotureCommande.java
+++ main/ClotureCommande.java
@@ -13,7 +13,7 @@ public class ClotureCommande {
     }
 
     public void cloturerCommande() {
-        System.out.println("\n=== ClÃƒÆ’Ã‚Â´ture d'une commande ===");
+        System.out.println("\n=== Cloture d'une commande ===");
         try {
             Scanner scanner = new Scanner(System.in);
             System.out.print("Voulez-vous afficher la liste des commandes en cours ? (1 = Oui / 0 = Non) : ");
@@ -25,9 +25,11 @@ public class ClotureCommande {
                                 "WHERE (c.idCommande = commandeenboutique.idCommande " +
                                 "       OR c.idCommande = commandealivrer.idCommande) " + 
                                 "  AND (commandeenboutique.statutcommandeb != 'Recuperee' " +
-                                "       AND commandeenboutique.statutcommandeb != 'AnnulÃƒÆ’Ã‚Â©e' " +
+                                "       AND commandeenboutique.statutcommandeb != 'Annulee' " +
                                 "       AND commandealivrer.statutcommandel != 'Livree' " +
-                                "       AND commandealivrer.statutcommandel != 'AnnulÃƒÆ’Ã‚Â©e')";
+                                "       AND commandealivrer.statutcommandel != 'Annulee')"+
+                                "       AND commandealivrer.statutcommandel != 'prete' " +
+                                "       AND commandeenboutique.statutcommandeb != 'prete' " ;
             PreparedStatement commandes = connection.prepareStatement(requete1);
             ResultSet rs = commandes.executeQuery();
 
@@ -38,12 +40,12 @@ public class ClotureCommande {
                 vide = false;
                 System.out.println("ID commande       : " + rs.getInt("idCommande"));
                 System.out.println("ID client         : " + rs.getInt("idClient"));
-                System.out.println("Mode rÃƒÆ’Ã‚Â©cupÃƒÆ’Ã‚Â©ration : " + rs.getString("ModeRecuperation"));
+                System.out.println("Mode récupération : " + rs.getString("ModeRecuperation"));
                 System.out.println("------------------------------");
             }
 
             if (vide) {
-                System.out.println("Aucune commande trouvÃƒÆ’Ã‚Â©e.");
+                System.out.println("Aucune commande trouvée.");
             }
                 
             }
@@ -56,7 +58,7 @@ public class ClotureCommande {
            
 
 
-            System.out.println("\nEntrez l'ID de la commande ÃƒÆ’  clÃƒÆ’Ã‚Â´turer :\n");
+            System.out.println("\nEntrez l'ID de la commande à clôturer :\n");
             System.out.print("ID commande : ");
             int id = Integer.parseInt(scanner.nextLine());
 
@@ -72,7 +74,7 @@ public class ClotureCommande {
                 modeRecup = rsMode.getString("ModeRecuperation");
             } else {
                 connection.rollback();
-                System.out.println("Commande non trouvÃƒÆ’Ã‚Â©e.");
+                System.out.println("Commande non trouvée.");
                 retour();
                 return;
             }
@@ -89,11 +91,11 @@ public class ClotureCommande {
                 psRetrait.executeUpdate();
             } else if( modeRecup.equals("Livraison") ) {
                 
-                statut = "En livraison" ;
+                statut = "Prete" ;
                 System.out.print("Distance de livraison (en km) : ");
                 int distance = Integer.parseInt(scanner.nextLine());
                 int FraisLivraison = (distance < 10) ? 5 : (10 + 10*((int)distance/100));
-                System.out.print("Date estimÃƒÆ’Ã‚Â©e de livraison (YYYY-MM-DD) : ");
+                System.out.print("Date estimée de livraison (YYYY-MM-DD) : ");
                 String dateEstime = scanner.nextLine();
                 requete = " Update CommandeaLivrer set StatutCommandeL = ? , FraisLivraison = ? , DateLivraisonEstimee = TO_DATE(?, 'YYYY-MM-DD') where idCommande = ? " ;
                 PreparedStatement psLivraison = connection.prepareStatement(requete);
@@ -104,68 +106,10 @@ public class ClotureCommande {
                 psLivraison.executeUpdate();   
             }
             else {
-                System.out.println("Mode de rÃƒÆ’Ã‚Â©cupÃƒÆ’Ã‚Â©ration invalide.");
+                System.out.println("Mode de récupération invalide.");
                 retour();
                 return ;
             }
-            connection.commit();
-            System.out.print(
-            "\n=== La commande est en cours de clÃƒÆ’Ã‚Â´ture : \n "+
-            "=== Tapez 0 pour retour \n " +
-            "=== Valider l'achat : V = validÃƒÆ’Ã‚Â©e, A = AnnulÃƒÆ’Ã‚Â©e .\n");
-            String key = scanner.nextLine().trim().toUpperCase();
-
-            if (key.equals("V") && modeRecup.equals("Retrait")) {
-                String req = "UPDATE Commandeenboutique SET StatutCommandeB = ? WHERE idCommande = ?";
-                PreparedStatement ps = connection.prepareStatement(req);
-                ps.setString(1, "Recuperee");
-                ps.setInt(2, id);
-                ps.executeUpdate();
-
-                System.out.println("Commande marquÃƒÆ’Ã‚Â©e comme RÃƒÆ’Ã‚Â©cupÃƒÆ’Ã‚Â©rÃƒÆ’Ã‚Â©e !");
-
-
-                
-            }
-
-            else if  (key.equals("V") && modeRecup.equals("Livraison")) {
-                String req = "UPDATE CommandeaLivrer SET StatutCommandeL = ? WHERE idCommande = ?";
-                PreparedStatement ps = connection.prepareStatement(req);
-                ps.setString(1, "Livree");
-                ps.setInt(2, id);
-                ps.executeUpdate();
-
-                System.out.println("Commande marquÃƒÆ’Ã‚Â©e comme LivrÃƒÆ’Ã‚Â©e !");
-                connection.commit();
-
-            }
-
-            else if (key.equals("A")) {
-                String req1 = "UPDATE Commandeenboutique SET StatutCommandeB = ? WHERE idCommande = ?";
-                String req2 = "UPDATE CommandeaLivrer SET StatutCommandeL = ? WHERE idCommande = ?";
-
-                PreparedStatement ps1 = connection.prepareStatement(req1);
-                ps1.setString(1, "AnnulÃƒÆ’Ã‚Â©e");
-                ps1.setInt(2, id);
-                ps1.executeUpdate();
-
-                PreparedStatement ps2 = connection.prepareStatement(req2);
-                ps2.setString(1, "AnnulÃƒÆ’Ã‚Â©e");
-                ps2.setInt(2, id);
-                ps2.executeUpdate();
-
-                System.out.println("Commande annulÃƒÆ’Ã‚Â©e !");
-                retour();
-                return;
-            }
-            else if ( key.equals("0") ) {
-                retour();
-                return ;    }
-            else {
-                connection.rollback();
-                System.out.println("Choix invalide. La commande n'a pas ÃƒÆ’Ã‚Â©tÃƒÆ’Ã‚Â© clÃƒÆ’Ã‚Â´turÃƒÆ’Ã‚Â©e.");
-                retour();
-                return; }
             String depiterStockp = "UPDATE LotProduit " + 
                             "       SET QuantiteDisponibleP = QuantiteDisponibleP - ( " + 
                             "           SELECT QuantiteCommandeeP " + 
@@ -203,7 +147,7 @@ public class ClotureCommande {
                             "       )";
 
             PreparedStatement psDepiterP = connection.prepareStatement(depiterStockp);
-            PreparedStatement psDepiterC = connection.prepareStatement(depiterStockc);   ;
+            PreparedStatement psDepiterC = connection.prepareStatement(depiterStockc);   
             psDepiterP.setInt(1, id)  ;
             psDepiterP.setInt(2, id);
             psDepiterC.setInt(1, id)  ;
@@ -211,18 +155,122 @@ public class ClotureCommande {
             psDepiterP.executeUpdate()  ;
             psDepiterC.executeUpdate()  ;
             connection.commit()  ;
+            System.out.print(
+            "\n=== Le stock est deputé : \n "+
+            "=== Tapez 0 pour retour \n " +
+            "=== Valider l'achat : V = validée, A = Annulée .\n");
+            String key = scanner.nextLine().trim().toUpperCase();
+
+            if (key.equals("V") && modeRecup.equals("Retrait")) {
+                String req = "UPDATE Commandeenboutique SET StatutCommandeB = ? WHERE idCommande = ?";
+                PreparedStatement ps = connection.prepareStatement(req);
+                ps.setString(1, "Recuperee");
+                ps.setInt(2, id);
+                ps.executeUpdate();
+
+                System.out.println("Commande marquée comme Récupérée !");
+
+
+                
+            }
+
+            else if  (key.equals("V") && modeRecup.equals("Livraison")) {
+                String req = "UPDATE CommandeaLivrer SET StatutCommandeL = ? WHERE idCommande = ?";
+                PreparedStatement ps = connection.prepareStatement(req);
+                ps.setString(1, "Livree");
+                ps.setInt(2, id);
+                ps.executeUpdate();
+
+                System.out.println("Commande marquée comme Livrée !");
+                
+
+            }
+
+            else if (key.equals("A")) {
+                String req1 = "UPDATE Commandeenboutique SET StatutCommandeB = ? WHERE idCommande = ?";
+                String req2 = "UPDATE CommandeaLivrer SET StatutCommandeL = ? WHERE idCommande = ?";
+
+                PreparedStatement ps1 = connection.prepareStatement(req1);
+                ps1.setString(1, "Annulée");
+                ps1.setInt(2, id);
+                ps1.executeUpdate();
+
+                PreparedStatement ps2 = connection.prepareStatement(req2);
+                ps2.setString(1, "Annulée");
+                ps2.setInt(2, id);
+                ps2.executeUpdate();
+
+                String restoreStockP =
+                "UPDATE LotProduit " +
+                "   SET QuantiteDisponibleP = QuantiteDisponibleP + ( " +
+                "       SELECT QuantiteCommandeeP " +
+                "       FROM LigneCommandeProduit " +
+                "       WHERE idCommande = ? " +
+                "         AND LigneCommandeProduit.idProduit = LotProduit.idProduit " +
+                "         AND LigneCommandeProduit.DateReceptionP = LotProduit.DateReceptionP " +
+                "         AND LigneCommandeProduit.ModeConditionnement = LotProduit.ModeConditionnement " +
+                "         AND LigneCommandeProduit.PoidsUnitaire = LotProduit.PoidsUnitaire " +
+                "   ) " +
+                " WHERE EXISTS ( " +
+                "       SELECT 1 FROM LigneCommandeProduit " +
+                "       WHERE idCommande = ? " +
+                "         AND LigneCommandeProduit.idProduit = LotProduit.idProduit " +
+                "         AND LigneCommandeProduit.DateReceptionP = LotProduit.DateReceptionP " +
+                "         AND LigneCommandeProduit.ModeConditionnement = LotProduit.ModeConditionnement " +
+                "         AND LigneCommandeProduit.PoidsUnitaire = LotProduit.PoidsUnitaire " +
+                "   )";
 
         
+                String restoreStockC =
+                    "UPDATE LotContenant " +
+                    "   SET QuantiteDisponibleC = QuantiteDisponibleC + ( " +
+                    "       SELECT QuantiteCommandeeC " +
+                    "       FROM LigneCommandeContenant " +
+                    "       WHERE idCommande = ? " +
+                    "         AND LigneCommandeContenant.idContenant = LotContenant.idContenant " +
+                    "         AND LigneCommandeContenant.DateReceptionC = LotContenant.DateReceptionC " +
+                    "   ) " +
+                    " WHERE EXISTS ( " +
+                    "       SELECT 1 FROM LigneCommandeContenant " +
+                    "       WHERE idCommande = ? " +
+                    "         AND LigneCommandeContenant.idContenant = LotContenant.idContenant " +
+                    "         AND LigneCommandeContenant.DateReceptionC = LotContenant.DateReceptionC " +
+                    "   )";
+
+                PreparedStatement psRestP = connection.prepareStatement(restoreStockP);
+                PreparedStatement psRestC = connection.prepareStatement(restoreStockC);
+
+                psRestP.setInt(1, id);
+                psRestP.setInt(2, id);
+                psRestC.setInt(1, id);
+                psRestC.setInt(2, id);
+
+                psRestP.executeUpdate();
+                psRestC.executeUpdate();
+
+                System.out.println("Commande annulée !");
+                retour();
+                return;
+            }
+            else if ( key.equals("0") ) {
+                retour();
+                return ;    }
+            else {
+                connection.rollback();
+                System.out.println("Choix invalide. La commande n'a pas été clôturée.");
+                retour();
+                return; }
+            connection.commit();
             
         }    
         catch (SQLException e) {
-            System.out.println("Erreur lors de la clÃƒÆ’Ã‚Â´ture de la commande : " + e.getMessage());
+            System.out.println("Erreur lors de la clôture de la commande : " + e.getMessage());
             retour();
             return;
         }
        
         
-        System.out.println("Commande clÃƒÆ’Ã‚Â´turÃƒÆ’Ã‚Â©e avec succÃƒÆ’Ã‚Â¨s !");
+        System.out.println("Commande clôturée avec succès !");
         
         retour();
     }
@@ -241,7 +289,7 @@ public class ClotureCommande {
             }    
             
             default -> {
-                System.out.println("Choix invalide, veuillez rÃƒÆ’Ã‚Â©essayer !");
+                System.out.println("Choix invalide, veuillez réessayer !");
             }
         }
     }
